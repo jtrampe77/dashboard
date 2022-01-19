05565358b45f708de829a991c1f370e2e928751a
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2022-01-19 10:01:36 -0700

    test

--- src/js/read.js
+++ src/js/read.js
@@ -1,4 +1,40 @@
- import { db } from "./libs/firebase/firebaseConfig";
+/*
+    Import data
+    Send data to Render function
+    RenderFunction(template, data) ==> component markup with data
 
-console.log(db)
+    1. Create template
+    2. Setup page init function async - fetch RTD data
+    3. Load the data - read page (update delete share same data...)
+*/
 
+import { ref, get } from "firebase/database";
+import { db } from "./libs/firebase/firebaseConfig";
+import {renderUser} from "./templates/user"
+
+let store = [];
+
+async function pageInit(){
+    
+    const userRef = ref(db,'users/');
+    const userSnapShot = await get(userRef);
+    const userData = {...userSnapShot.val()};
+
+    /*
+         extract the keys of each data
+         Object
+            obj.prop
+    */
+
+    const users = Object.keys(userData).map(user=>{
+        return(userData[user])
+    })
+
+    console.log(users)
+
+  
+
+    
+}
+
+pageInit()
